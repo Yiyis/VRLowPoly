4090d70943993bcde6dcbd745c2d6f4002435d1f
Yiyi <414956851@qq.com>
2019-01-08 10:20:09 -0500

    adjusted and optimized scene, added interactable object

---
+++ Assets/Scripts/ConstantRotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantRotate : MonoBehaviour {
+    #region variables
+    #endregion
+
+    #region accessors
+    #endregion
+
+    #region functions
+
+    // Use this for initialization
+    void Start () {
+        transform.Rotate(-15, 0, 15);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, Time.deltaTime * 10, 0);
+	} 
+    #endregion 
+}
+   
+
